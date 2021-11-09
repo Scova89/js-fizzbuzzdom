48f7163858e2ee4d85ae2ba2aa3b3a9bf6d4b11a
Francesco <scovascova@gmail.com>
2021-11-09 15:58:19 +0100

    fuzz buzz e fuzzbuzz

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 // alert('ciao')
+const box = document.getElementById('box')
+let z = 0
 
 for(let i = 1; i <= 100; i++){
-    console.log(i);
+    if(i % 3 == 0){
+        x= 'Fizz';
+    }else if(i % 5 == 0){
+        x= 'Buzz';
+    }else if(i % 15 == 0){
+        x= 'FizzBuzz';    
+    }else{
+        x=i; 
+    }
+    console.log(x);
 }

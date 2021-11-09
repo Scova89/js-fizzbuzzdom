59ca927d7cf31b3238680cf976a1ecac7c65b1d4
Francesco <scovascova@gmail.com>
2021-11-09 16:46:42 +0100

    box colorati

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
 // alert('ciao')
-const box = document.getElementById('box')
-let z = 0
+const box = document.getElementById('box');
+let x = 0;
 
 for(let i = 1; i <= 100; i++){
-    if(i % 3 == 0){
+    if(i % 15 == 0){
+        x= 'FizzBuzz';
+    } else if(i % 3 == 0){
         x= 'Fizz';
     }else if(i % 5 == 0){
         x= 'Buzz';
-    }else if(i % 15 == 0){
-        x= 'FizzBuzz';    
     }else{
         x=i; 
     }
     console.log(x);
+
+    if (isNaN(x)){
+        // se x è una stringa entri
+        box.innerHTML += `<li class='box ${x}'>${x}</li>`;
+    }else{
+        box.innerHTML += `<li class='box'>${x}</li>`;
+    }
 }
+
+
+
